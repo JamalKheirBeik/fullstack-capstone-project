e78611ae5fe35e074ab8dff136267b2cf0349a52
Jamal Kheir Beik <42125070+JamalKheirBeik@users.noreply.github.com>
2024-03-18 14:25:49 +0300

    Update app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -12,7 +12,7 @@ const corsOptions = {
     origin: '*',
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200,
-}
+};
 app.use("*", cors(corsOptions));
 const port = 3060;
 
@@ -55,8 +55,8 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-    res.send("Inside the server")
-})
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
